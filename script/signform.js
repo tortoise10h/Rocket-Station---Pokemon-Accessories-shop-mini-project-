683c6ad3b9398e3c9af3d911f408b2e415eeb984
tortoise10h <tortoise10h@gmai.com>
2018-11-20 16:54:52 +0700

    add login form and modified font size of sign button

--- script/signform.js
+++ script/signform.js
@@ -1,22 +1,31 @@
+//For sign up zone
 let signUpBtn = document.getElementById('sign-up-btn');
-
 let signUpBox = document.getElementById('sign-up-box');
+let closeSignUpBtn = document.getElementById('close-sign-up');
+let cancelSignUpBtn = document.getElementById('cancel-sign-up');
+signUpBtn.addEventListener('click', openSignUpBox);
+closeSignUpBtn.addEventListener('click', hideSignUpBox);
+cancelSignUpBtn.addEventListener('click', hideSignUpBox);
 
-let closeBtn = document.getElementById('close-sign-up');
+//for sign in zone
+let signInBtn = document.getElementById('sign-in-btn');
+let signInBox = document.getElementById('sign-in-box');
+let closeSigninBtn = document.getElementById('close-sign-in');
+let cancelSigninBtn = document.getElementById('cancel-sign-in');
+signInBtn.addEventListener('click', openSignInBox);
+closeSigninBtn.addEventListener('click', hideSignInBox);
+cancelSigninBtn.addEventListener('click', hideSignInBox);
 
-let cancelBtn = document.getElementById('cancel-sign-up');
-
+//get related zone
 let searchZone = document.getElementById('search-zone');
 
 let slide = document.getElementById('slide-zone');
 
 let navZone = document.getElementById('nav-id');
 
-signUpBtn.addEventListener('click', openSignUpBox);
 
-closeBtn.addEventListener('click', hideSignUpBox);
 
-cancelBtn.addEventListener('click', hideSignUpBox);
+
 
 function openSignUpBox(){
     signUpBox.style.display = "block";
@@ -26,6 +35,19 @@ function openSignUpBox(){
 }
 function hideSignUpBox(){
     signUpBox.style.display = "none";
+    searchZone.style.display = "grid";
+    slide.style.display = "block";
+    navZone.style.display = "grid";
+}
+
+function openSignInBox(){
+	signInBox.style.display = "block";
+    searchZone.style.display = "none";
+    slide.style.display = "none";
+    navZone.style.display = "none";
+}
+function hideSignInBox(){
+    signInBox.style.display = "none";
     searchZone.style.display = "grid";
     slide.style.display = "block";
     navZone.style.display = "grid";
